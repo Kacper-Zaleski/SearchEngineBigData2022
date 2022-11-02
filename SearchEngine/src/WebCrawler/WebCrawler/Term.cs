f39426795066516bafa65f66b806b655b546db09
Kacper <bmx.kacper@gmail.com>
2022-11-02 23:31:51 +0000

    #Added Docker and analyzer

---
+++ SearchEngine/src/WebCrawler/WebCrawler/Term.cs
@@ -0,0 +1,24 @@
+﻿using Indexer.Models;
+
+namespace SearchEngine.Calculation.Calculation
+{
+    public class Term
+    {
+        private int _allPageCount { get; set; }
+
+        public double idf
+        {
+            get => Math.Log10((double)_allPageCount / (double)PageToTermFrequency.Count);
+        }
+
+        public Term(int number)
+        {
+            _allPageCount = number;
+            PageToTermFrequency = new Dictionary<Page, int>();
+        }
+
+        public Dictionary<Page, int> PageToTermFrequency;
+
+        public double GetTfidf(Page p) => (1 + Math.Log10(PageToTermFrequency[p])) * idf;
+    }
+}
